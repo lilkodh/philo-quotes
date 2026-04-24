3d9c0dfab0bf77fdaefef95df6552495add1582f
lilkodh <khaliddrihemdev@gmail.com>
2026-04-24 09:04:56 +0100

    day-5

--- script.js
+++ script.js
@@ -5,8 +5,7 @@ const quoteauthor = document.getElementById('quote-author');
 const favoriteText = document.getElementById('favorite-text');
  async function fINDQuote() {
     const name = Inputtext.value;
-    
-    if(name === "") {
+         if(name === "") {
         quotetext.textContent= "write a name please "
         return;
     } 
@@ -14,13 +13,17 @@ const favoriteText = document.getElementById('favorite-text');
     quoteauthor.textContent= ""
    try {
 
-    const respone = await fetch ('https://api-wisdom.deontex.com/api/v1/quotes?search=&philosopher_id=&topic=&limit=20&offset=0');
+    const respone = await fetch(`https://api-wisdom.deontex.com/api/v1/quotes?search=?${name}`)
     const allQuotes = await respone.json();
-        const found = allQuotes.find(q => q.author.toLowerCase().includes(name.toLowerCase()));
-
+        const found = allQuotes.data.filter(q => q.philosopher_name);
+        console.log(found.length)
+        
+        const dispalyQuote = Math.floor(Math.random()*found.length)
+        console.log(dispalyQuote);
+        
     if(found){
-        quotetext.textContent =  found.quote
-        quoteauthor.textContent= found.author
+        quotetext.textContent =  found[dispalyQuote].quote_text
+        quoteauthor.textContent= found[dispalyQuote].philosopher_name
     } else{
          quotetext.textContent = "we did not found"
     }
